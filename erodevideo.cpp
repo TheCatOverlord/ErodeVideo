81d003062eb53e036d319b796b5702b2fff1cdad
FireWire | Spencer C <sgcollet12@gmail.com>
2019-07-29 14:53:01 -0400

    Added comments
    Added some comments to make it look better

--- erodevideo.cpp
+++ erodevideo.cpp
@@ -4,29 +4,42 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv){
-    VideoCapture cap(0);\
+    // Start the webcam and check if opened
+    VideoCapture cap(0);
     int sizeOfKernel = 3;
     if (cap.isOpened() == false){
         cout << "[Failure] Failed to open video feed." << endl;
         return 1;
     }
+
+    // Define names for windows
     String windowNameOriginal = "Original video feed";
     String windowNameErode = "Eroded video feed";
+    //Create the windows
     namedWindow(windowNameOriginal, WINDOW_NORMAL);
     namedWindow(windowNameErode, WINDOW_NORMAL);
     cout << "The current kernel size is " << sizeOfKernel << endl;
 
     while (true){
+        // Image arrays
         Mat frame;
         Mat frameEroded;
+
+        // Read frame and check is worked
         bool bSuccess = cap.read(frame);
         if(bSuccess == false){
             cout << "[Failure] Failed to grab frame. (Disconnected?)" << endl;
             break;
         }
+
+        // Erode the image
         erode(frame, frameEroded, getStructuringElement(MORPH_ERODE, Size(sizeOfKernel,sizeOfKernel)));
+
+        // Display the images in the window
         imshow(windowNameOriginal, frame);
         imshow(windowNameErode, frameEroded);
+
+        // Check for keystrokes in the windows and handle them
         char key = waitKey(10);
         if (key == 27){
             cout << "[Note] ESC key was pressed by the user. Stopping video feed." << endl;
@@ -53,6 +66,8 @@ int main(int argc, char** argv){
             }
         }
     }
+
+    // Destroy all windows and exit
     destroyAllWindows();
     return 0;
 }
